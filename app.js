93922a046e2674e3dd6903e00f8f921e1fee8e14
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-07-17 19:04:47 -0500

    get function working

--- app.js
+++ app.js
@@ -3,21 +3,45 @@ require("dotenv").config();
 
 const Twit = require("twit");
 
+const Twitter = require("twitter");
+
 const keys = require("./keys");
 
-const T = new Twit(keys.twit);
+const TwitPost = new Twit(keys.twit);
 
-let tweet = {
-    status: "Tweet from Node.js!"
+const T = new Twitter(keys.twitter);
+
+// let tweet = {
+//     status: "Tweet from Node.js!"
+// }
+
+
+// let postTweet = T.post("statuses/update", tweet, tweeted)
+
+// function tweeted(error, tweet, response) {
+//     if (error) {
+//         console.log("ERROR: " + error);
+//     } else {
+//         console.log("It worked!");
+//     }
+// }
+
+let params = {
+    screen_name: "bandanaCoder",
+    count: 20
 }
 
+function getTweets() {
+    T.get("statuses/user_timeline", params, gotTweets);
+}
 
-T.post("statuses/update", tweet, tweeted)
-
-function tweeted(error, tweet, response) {
+function gotTweets(error, data, response) {
     if (error) {
-        console.log("ERROR: " + error)
+        console.log("ERROR: " + error);
     } else {
-        console.log("It worked!");
+        let tweetLength = data.length;
+        for (i = 0; i < tweetLength; i++) {
+            console.log(JSON.stringify(data[i].text + " --- POSTED ON: " + data[i].created_at, null, 2))
+        }
     }
 }
